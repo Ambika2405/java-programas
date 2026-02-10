d9ddee14411f0405630e38631ca1ec44f14f95da
Ambika <ambikahulsure5@gmail.com>
2026-02-10 10:55:35 +0530

    Add program to reverse a given number using loops

---
+++ p2.java
@@ -0,0 +1,15 @@
+//Write down the program to reverse the given numbers using loops
+class ReverseNumber {
+    public static void main(String[] args) {
+        int num = 876;
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        System.out.println("Reversed number: " + reverse);
+    }
+}
