5c071593d42572366cc065a79e8a903ef6bcceeb
Ambika <ambikahulsure5@gmail.com>
2026-02-10 10:57:35 +0530

    Add program to check if a number is positive or negative

---
+++ p3.java
@@ -0,0 +1,11 @@
+//Write a program to find the given number is negative or positive input=123
+class PositiveOrNegative {
+    public static void main(String[] args) {
+        int num = 123;
+
+        if (num >= 0)
+            System.out.println("Positive number");
+        else
+            System.out.println("Negative number");
+    }
+}
