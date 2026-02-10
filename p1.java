a77dbed814b72cf7c9cec8ccf9f3d2dce3a52497
Ambika <ambikahulsure5@gmail.com>
2026-02-10 10:52:49 +0530

    Fix loop to print numbers from 10 to 50
    The loop incorrectly counts down from -10 to -50 instead of from 10 to 50.

---
+++ p1.java
@@ -0,0 +1,10 @@
+//Write a program to print the number from 10 to 50 using for loop
+class PrintNumbersFor {
+    public static void main(String[] args) {
+
+        for (int i = -10; i >= -50; i--) {
+            System.out.println(i);
+        }
+
+    }
+}
