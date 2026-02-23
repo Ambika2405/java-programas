e8157fdf52e59b97d62f1bd43fb8fa2c83ca31a1
Ambika <ambikahulsure5@gmail.com>
2026-02-23 12:30:25 +0530

    Refactor payment classes to use constructor and override

--- Payment using classes by inheritance.java
+++ Payment using classes by inheritance.java
@@ -1,24 +1,36 @@
 //for the payment program class name should be payment.Create a child classes use @override method to inherit from the parent class. don't use extends
-interface Payment{
-  void pay(double amount);
-}
-class CashPayment implements Payment{
-  @Override
-  public void pay(double amount){
-    System.out.println("cash payment of:"+ amount);
+class Payment{
+  double amount;
+  Payment(double amount){
+    this.amount = amount;
+  }
+  void pay(){
+    System.out.println("Processing payment..");
   }
 }
-class CardPayment implements Payment{
+class CardPayment extends Payment{
+  CardPayment(double amount){
+    super(amount);
+  }
   @Override
-  public void pay(double amount){
-    System.out.println("card payment of:" +amount);
+  void pay(){
+    System.out.println("Paid " +amount+"using Card");
+  }
+}
+class CashPayment extends Payment{
+  CashPayment(double amount){
+  super(amount);
+}
+  @Override
+  void pay(){
+   System.out.println("Paid" +amount+"using Cash");
   }
 }
 public class Main {
   public static void main(String[] args){
-    Payment p1=new CashPayment();
-    Payment p2=new CardPayment();
-    p1.pay(500);
-    p2.pay(1000);
+    Payment p1=new CardPayment(2000);
+    Payment p2=new CashPayment(500);
+    p1.pay();
+    p2.pay();
   }
 }
