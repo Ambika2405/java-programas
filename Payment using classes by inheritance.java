b455f1dd563b51aa00ceea33430d0da7530b8c93
Ambika <ambikahulsure5@gmail.com>
2026-02-23 11:15:00 +0530

    Implement payment system using interface and classes

--- Payment using classes by inheritance.java
+++ Payment using classes by inheritance.java
@@ -1,1 +1,24 @@
 //for the payment program class name should be payment.Create a child classes use @override method to inherit from the parent class. don't use extends
+interface Payment{
+  void pay(double amount);
+}
+class CashPayment implements Payment{
+  @Override
+  public void pay(double amount){
+    System.out.println("cash payment of:"+ amount);
+  }
+}
+class CardPayment implements Payment{
+  @Override
+  public void pay(double amount){
+    System.out.println("card payment of:" +amount);
+  }
+}
+public class Main {
+  public static void main(String[] args){
+    Payment p1=new CashPayment();
+    Payment p2=new CardPayment();
+    p1.pay(500);
+    p2.pay(1000);
+  }
+}
