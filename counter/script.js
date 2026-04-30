f0a2ba414d4345f1b7c0ca3992d74b2cbdee085c
Ambika <ambikahulsure5@gmail.com>
2026-04-30 11:25:27 +0530

    Add files via upload

---
+++ counter/script.js
@@ -0,0 +1,32 @@
+let count = 0;
+
+const countDisplay = document.getElementById("count");
+
+function updateDisplay() {
+    countDisplay.textContent = count;
+
+    if (count > 0) {
+        countDisplay.style.color = "green";
+    } else if (count < 0) {
+        countDisplay.style.color = "red";
+    } else {
+        countDisplay.style.color = "#e0e1dd";
+    }
+}
+
+function increase() {
+    count++;
+    updateDisplay();
+}
+
+function decrease() {
+    count--;
+    updateDisplay();
+}
+
+function reset() {
+    count = 0;
+    updateDisplay();
+}
+
+updateDisplay();
